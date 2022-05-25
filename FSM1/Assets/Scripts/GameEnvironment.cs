d915fe068ba6ac5d1c59ea4c8b52e9d5548381fc
GabrieleFederico <gabrielefederico@live.it>
2022-05-25 21:13:58 +0200

    FSM lesson 1
    It is cool to see how such a theoretical concept like Finite State Machines actually does have a use like, also seeing singleton pattern being used once again after a long time is always a nice reminder of its existance

---
+++ FSM1/Assets/Scripts/GameEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnvironment : MonoBehaviour
+{
+
+    private static GameEnvironment istance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } };
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (istance == null)
+            {
+                istance = new GameEnvironment();
+                istance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return istance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
