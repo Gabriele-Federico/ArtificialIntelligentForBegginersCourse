a520dc7a0244c4125d0cad80971dad8bf23a6b8a
GabrieleFederico <gabrielefederico@live.it>
2022-05-25 17:11:10 +0200

    Completer the first NavMesh tutorial
    This is really interesting, I wonder how navmeshes are actually made, might try to look up the code if it is open source

--- Waypoints&Graphs/Assets/Scipts/FollowPath.cs
+++ Waypoints&Graphs/Assets/Scipts/FollowPath.cs
@@ -5,35 +5,30 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
 
-    Transform goal;
-    float speed = 5f;
-    float accuracy = 1f;
-    float rotSpeed = 2f;
     public GameObject wpManager;
     GameObject[] wps;
-    GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
+    UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
-        currentNode = wps[0];
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     public void GoToHeli()
     {
-        g.AStar(currentNode, wps[1]);
-        currentWP = 0;
+        agent.SetDestination(wps[1].transform.position);
+        //g.AStar(currentNode, wps[1]);
+        //currentWP = 0;
 
     }
 
     public void GoToRuin()
     {
-        g.AStar(currentNode, wps[6]);
-        currentWP = 0;
+        agent.SetDestination(wps[6].transform.position);
+        //g.AStar(currentNode, wps[6]);
+        //currentWP = 0;
     }
 
     // Update is called once per frame
@@ -44,21 +39,6 @@ public class FollowPath : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength()) return;
-
-        currentNode = g.getPathPoint(currentWP);
-
-        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy) currentWP++;
-        
-        if(currentWP < g.getPathLength())
-        {
-            goal = g.getPathPoint(currentWP).transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.x);
-            Vector3 direction = lookAtGoal - transform.position;
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
+      
     }
 }
