1c97789ecb80dc1a6ffde3e065d7527039df4b2d
GabrieleFederico <gabrielefederico@live.it>
2022-05-26 13:13:59 +0200

    Fleeing

--- CityCrowd/Assets/AIControl.cs
+++ CityCrowd/Assets/AIControl.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
     GameObject[] goalLocations;
-    UnityEngine.AI.NavMeshAgent agent;
+    NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 5f;
+    float fleeRadius = 10f;
 
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.1f, 1.5f);
+        agent.speed = 2 * speedMult;
+        agent.angularSpeed = 120;
+        anim.SetFloat("speedMult", speedMult);
+        anim.SetTrigger("isWalking");
+        agent.ResetPath();
+    }
 
     // Use this for initialization
     void Start() {
@@ -13,14 +26,36 @@ public class AIControl : MonoBehaviour {
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
-        anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        ResetAgent();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if(Vector3.Distance(position, transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (transform.position - position).normalized;
+            Vector3 newGoal = transform.position + fleeDirection * fleeRadius;
+            
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+            if(path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update() {
 
-        if (agent.remainingDistance < 1) {
-
+        if (agent.remainingDistance < 1) 
+        {
+            ResetAgent();   
             agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         }
     }
---
+++ CityCrowd/Assets/DropCilynder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCilynder : MonoBehaviour
+{
+    public GameObject obstacle;
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("agent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit info;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out info))
+            {
+                Instantiate(obstacle, info.point, obstacle.transform.rotation);
+                foreach(GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().DetectNewObstacle(info.point);
+                }
+            }
+        }
+    }
+
+}
