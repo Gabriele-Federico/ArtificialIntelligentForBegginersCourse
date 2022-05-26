d15a10b2518908a307635659ea9f8d6a5b503e57
GabrieleFederico <gabrielefederico@live.it>
2022-05-26 14:25:36 +0200

    Flocking part 1
    Just boids

---
+++ AIFishFlocking/Assets/FlockManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+
+    [Header("Fish Settings")]
+    [Range(0f, 5f)]
+    public float minSpeed;
+    [Range(0f, 5f)]
+    public float maxSpeed;
+    [Range(1f, 10f)]
+    public float neighbourDistance;
+    [Range(0f, 5f)]
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for(int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), 
+                                                           Random.Range(-swimLimits.y, swimLimits.y),
+                                                           Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
