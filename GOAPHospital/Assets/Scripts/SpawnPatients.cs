2436723ebbb7a99e71d292df0ceaea55e46703e9
GabrieleFederico <gabrielefederico@live.it>
2022-05-28 18:43:58 +0200

    MultiStepPlan
    Very interesting, I would like to try some more complex stuff, but I gotta go step by step

---
+++ GOAPHospital/Assets/Scripts/SpawnPatients.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPatients : MonoBehaviour
+{
+
+    public GameObject patientPrefab;
+    public int numPatients;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnPatient", 5);
+    }
+
+    void SpawnPatient()
+    {
+        Instantiate(patientPrefab, transform.position, Quaternion.identity);
+        Invoke("SpawnPatient", Random.Range(2, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
