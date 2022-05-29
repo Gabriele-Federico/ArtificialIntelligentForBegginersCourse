c948644b47985f80cf82628134fb32e93b5b372a
GabrieleFederico <gabrielefederico@live.it>
2022-05-29 10:04:55 +0200

    Multi Agents plans

--- GOAPHospital/Assets/Scripts/GWorld.cs
+++ GOAPHospital/Assets/Scripts/GWorld.cs
@@ -7,15 +7,29 @@ public sealed class GWorld
 
     private static readonly GWorld instance = new GWorld();
     private static WorldStates world;
+    private static Queue<GameObject> patients;
 
     static GWorld()
     {
         world = new WorldStates();
+        patients = new Queue<GameObject>();
     }
 
     private GWorld() 
     {
         
+    }
+
+    public void AddPatient(GameObject patient)
+    {
+        patients.Enqueue(patient);
+    }
+
+    public GameObject RemovePatient()
+    {
+        if (patients.Count == 0)
+            return null;
+        return patients.Dequeue();
     }
 
     public static GWorld Instance { get { return instance; } }
