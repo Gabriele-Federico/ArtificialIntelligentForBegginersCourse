f3f21a3dabdfd360ddbde304636ce2cbdd662b6b
GabrieleFederico <gabrielefederico@live.it>
2022-05-30 10:06:04 +0200

    Completed the course
    Also added something more to the little challenge so that is more "scalable" in case I want to make him steal other objects with different values, for example I might have a list of all the stealable objects and pick one randomly then make him steal objects until he has enough money, also I could make it Random if he steals it successfully or not and if he doesn't make an alarm and the robber has to run to the van without stealing

--- BehaviourTrees/Assets/Scripts/RobberBehaviour.cs
+++ BehaviourTrees/Assets/Scripts/RobberBehaviour.cs
@@ -12,11 +12,15 @@ public class RobberBehaviour : MonoBehaviour
     public GameObject backDoor;
     public GameObject frontDoor;
     NavMeshAgent agent;
+    GameObject heldItem;
 
     public enum ActionState { IDLE, WORKING };
     ActionState state = ActionState.IDLE;
 
     Node.Status treeStatus = Node.Status.RUNNING;
+
+    [Range(0, 1000)]
+    public int money = 800;
 
     // Start is called before the first frame update    
     void Start()
@@ -26,14 +30,16 @@ public class RobberBehaviour : MonoBehaviour
         tree = new BehaviourTree();
         Sequence steal = new Sequence("Steal Something");
         Leaf goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
+        Leaf hasGotMoney = new Leaf("Has Got Money", HasMoney);
         Leaf goToVan = new Leaf("Go To Van", GoToVan);
         Leaf goToBackDoor = new Leaf("Go To Back Door", GoToBackDoor);
         Leaf goToFrontDoor = new Leaf("Go To Back Door", GoToFrontDoor);
         Selector openDoor = new Selector("Open Door");
 
-        openDoor.AddChild(goToBackDoor);
         openDoor.AddChild(goToFrontDoor);
+        openDoor.AddChild(goToBackDoor);
 
+        steal.AddChild(hasGotMoney);
         steal.AddChild(goToBackDoor);
         steal.AddChild(goToDiamond);
         //steal.AddChild(goToBackDoor);
@@ -42,27 +48,75 @@ public class RobberBehaviour : MonoBehaviour
         tree.PrintTree();
 
         tree.Process();
+
+    }
+
+    public Node.Status HasMoney()
+    {
+        if(money >= 500)
+            return Node.Status.FAILURE;
+        return Node.Status.SUCCESS;
     }
 
 
     public Node.Status GoToDiamond()
     {
-        return GoToLocation(diamond.transform.position);
+        Node.Status s = GoToLocation(diamond.transform.position);
+        if (s == Node.Status.SUCCESS)
+        {
+            diamond.transform.parent = gameObject.transform;
+            heldItem = diamond;
+        }
+        return s;
     }
 
     public Node.Status GoToVan()
     {
-        return GoToLocation(van.transform.position);
+        Node.Status s = GoToLocation(van.transform.position);
+        if(s == Node.Status.SUCCESS)
+        {
+            s = SellItem(heldItem);
+        }
+        return s;
+    }
+
+    public Node.Status SellItem(GameObject item)
+    {
+        ValuableItem vi = item.GetComponent<ValuableItem>();
+        if (vi != null)
+        {
+            money += vi.value;
+            Destroy(item);
+            heldItem = null;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.FAILURE;
     }
 
     public Node.Status GoToBackDoor()
     {
-        return GoToLocation(backDoor.transform.position);
+        return GoToDoor(backDoor);
     }
 
     public Node.Status GoToFrontDoor()
     {
-        return GoToLocation(frontDoor.transform.position);
+        return GoToDoor(frontDoor);
+    }
+
+    public Node.Status GoToDoor(GameObject door)
+    {
+        Node.Status s = GoToLocation(door.transform.position);
+        if (s == Node.Status.SUCCESS)
+        {
+            if (!door.GetComponent<Lock>().isLocked)
+            {
+                door.SetActive(false);
+                return Node.Status.SUCCESS;
+            }
+            return Node.Status.FAILURE;
+        }
+        else
+            return s;
     }
 
     Node.Status GoToLocation(Vector3 destination)
@@ -89,7 +143,12 @@ public class RobberBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (treeStatus == Node.Status.RUNNING)
+        if (treeStatus != Node.Status.SUCCESS)
             treeStatus = tree.Process(); 
+    }
+
+    bool isHolding()
+    {
+        return heldItem != null;
     }
 }
